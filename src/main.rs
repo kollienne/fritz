07cb66aa98d1470c2346bc7dc2882892ae0f0229
Kieran Collienne <20397647+kollienne@users.noreply.github.com>
2024-03-27 13:28:34 -0700

    borrow ast parsing from nix_editor

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use serde::{Serialize, Deserialize};
 use figment::{Figment, providers::{Serialized, Toml, Env, Format}};
 use rnix::{self, SyntaxKind, SyntaxNode};
 use nix_editor;
+use std::fs;
 
 #[derive(Parser, Debug, Serialize, Deserialize)]
 #[command(version, about, long_about = None)]
@@ -35,19 +36,94 @@ impl Default for Config {
     }
 }
 
-fn get_current_packages(config_file: &String) -> Result<String, nix_editor::read::ReadError> {
+fn get_current_packages(config_file: &String) -> Option<SyntaxNode> {
+    // let content = fs::read_to_string(config_file)?; 
+    let content = match fs::read_to_string(config_file) {
+        Ok(content) => content,
+        Err(err) => {
+            eprintln!("error reading file: {}", err);
+            return None;
+        }
+    };
+    let parsed = rnix::Root::parse(&content);
     let ast = rnix::Root::parse(config_file);
     let sn = &ast.syntax();
-    let kind = sn.kind();
-    let configbase = match nix_editor::parse::getcfgbase(&ast.syntax()) {
+    let expr = match ast.tree().expr() {
+        Some(expr) => expr,
+        None => {
+            eprintln!("error reading file");
+            return None;
+        }
+    };
+
+    let configbase = match nix_editor::parse::getcfgbase(&parsed.syntax()) {
         Some(x) => x,
         None => {
             eprintln!("could not parse {}", config_file);
-            return Err(nix_editor::read::ReadError::ParseError)
+            return None;
         }
     };
-    let current_packages = nix_editor::read::readvalue(&config_file, &"home.packages");
-    current_packages
+    let packages = match nix_editor::parse::findattr(&configbase, &"home.packages") {
+        Some(attr) => attr,
+        None => {
+            return None;
+        }
+    };
+    Some(packages)
+}
+
+fn addtoarr_aux(node: &SyntaxNode, items: Vec<String>) -> Option<SyntaxNode> {
+    for child in node.children() {
+        if child.kind() == rnix::SyntaxKind::NODE_WITH {
+            return addtoarr_aux(&child, items);
+        }
+        if child.kind() == SyntaxKind::NODE_LIST {
+            let mut green = child.green().into_owned();
+
+            for elem in items {
+                let mut i = 0;
+                for c in green.children() {
+                    if c.to_string() == "]" {
+                        if green.children().collect::<Vec<_>>()[i - 1]
+                            .as_token()
+                            .unwrap()
+                            .to_string()
+                            .contains('\n')
+                        {
+                            i -= 1;
+                        }
+                        green = green.insert_child(
+                            i,
+                            rnix::NodeOrToken::Node(
+                                rnix::Root::parse(&format!("\n{}{}", " ".repeat(4), elem))
+                                    .syntax()
+                                    .green()
+                                    .into_owned(),
+                            ),
+                        );
+                        break;
+                    }
+                    i += 1;
+                }
+            }
+
+            let index = match node.green().children().position(|x| match x.into_node() {
+                Some(x) => x.to_owned() == child.green().into_owned(),
+                None => false,
+            }) {
+                Some(x) => x,
+                None => return None,
+            };
+
+            let replace = node
+                .green()
+                .replace_child(index, rnix::NodeOrToken::Node(green));
+            let out = node.replace_with(replace);
+            let output = rnix::Root::parse(&out.to_string()).syntax();
+            return Some(output);
+        }
+    }
+    None
 }
 
 fn main() {
@@ -58,23 +134,32 @@ fn main() {
         .extract().unwrap();
 
     println!("reading config file: {}", config.hm_config_file);
-    let current_config = get_current_packages(&config.hm_config_file);
-
-    match current_config {
-        Err(e) => {
+    let cf_path = &config.hm_config_file;
+    let current_packages = match get_current_packages(&config.hm_config_file) {
+        Some(cfg) => {
+            cfg
+        },
+        None => {
             eprintln!("Unable to read config file {}", config.hm_config_file);
-            eprintln!("Error: {:#?}", e);
             std::process::exit(1);
         },
-        Ok(cfg) => {
-            
-        }
-    }
+    };
+
+    println!("current packages: {}", current_packages);
+    let current_packages_string = current_packages.to_string();
 
     let cli_args = Cli::parse();
     match cli_args.command {
         Commands::Add {pkg} => {
             println!("Adding package {}", pkg);
+            let new_str = match addtoarr_aux(&current_packages, vec![pkg]) {
+                Some(new_str) => new_str,
+                None => {
+                    eprintln!("error adding package");
+                    std::process::exit(1);
+                }
+            };
+            println!("{}", new_str);
         }
     }
 }
